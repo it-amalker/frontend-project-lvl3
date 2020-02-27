cc17c26abc2bd72314ecbc92875ea3416fa3cc96
amalker <it.amalker@yandex.ru>
2020-02-27 18:57:31 +0300

    add en locales

---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    errors: {
+      emptyInput: 'Empty input field not allowed',
+      feedAlreadyExist: 'Inputed feed <b>{{ value }}</b> already in the feeds list below',
+      invalidUrl: 'Invalid RSS feed URL. Example of feed URL: <b>http://example.com/feed</b>',
+      networkIssue: 'Network problems, try again.',
+    },
+    alerts: {
+      problem: 'Ouch...Houston, we’ve had a problem:',
+      success: 'Feed channel successfully added to the list',
+    },
+  },
+};
