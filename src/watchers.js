b9bcc9d9d1f925ef0b03d7fa07953cba326d8c61
amalker <it.amalker@yandex.ru>
2020-02-29 18:27:20 +0300

    update alertRender logic

--- src/watchers.js
+++ src/watchers.js
@@ -1,6 +1,6 @@
 import { watch } from 'melanke-watchjs';
 import i18next from 'i18next';
-import generateFeedCard from './componets';
+import generateFeedCards from './componets';
 
 const renderErrors = (errors) => {
   const errorNames = Object.keys(errors);
@@ -18,12 +18,20 @@ const renderErrors = (errors) => {
   }
 };
 
-const renderSuccess = () => {
+const renderAlert = (state) => {
   const alertContainer = document.querySelector('.alert-container');
+  const removeAlert = () => {
+    if (alertContainer.childNodes) {
+      alertContainer.innerHTML = '';
+    }
+  };
+  removeAlert();
   const div = document.createElement('div');
-  div.classList.add('alert', 'alert-success', 'pb-0', 'pt-0', 'mb-1');
-  div.innerHTML = `<strong>${i18next.t('alerts.success')}</strong>`;
+  const alertType = state.form.processState === 'sending' ? 'primary' : 'success';
+  div.classList.add('alert', `alert-${alertType}`, 'pb-0', 'pt-0', 'mb-1');
+  div.innerHTML = `<strong>${i18next.t(`alerts.${state.form.processState}`)}</strong>`;
   alertContainer.appendChild(div);
+  setTimeout(removeAlert, 5 * 1000);
 };
 
 export default (state) => {
@@ -50,7 +58,7 @@ export default (state) => {
 
   watch(state, 'updated', () => {
     if (state.updated === false) {
-      generateFeedCard(state.feeds, state.posts);
+      generateFeedCards(state.feeds, state.posts);
     }
   });
 
@@ -64,10 +72,11 @@ export default (state) => {
         break;
       case 'sending':
         submitButton.disabled = true;
+        renderAlert(state);
         break;
       case 'finished':
-        generateFeedCard(state.feeds, state.posts);
-        renderSuccess(state);
+        renderAlert(state);
+        generateFeedCards(state.feeds, state.posts);
         break;
       default:
         throw new Error(`Unknown state: ${processState}`);
