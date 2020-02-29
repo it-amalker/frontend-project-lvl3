8e8122620635f87eb420cbc64132bd8850b4145d
amalker <it.amalker@yandex.ru>
2020-02-29 18:23:36 +0300

    add new alert text

--- src/locales/en.js
+++ src/locales/en.js
@@ -4,11 +4,13 @@ export default {
       emptyInput: 'Empty input field not allowed',
       feedAlreadyExist: 'Inputed feed <b>{{ value }}</b> already in the feeds list below',
       invalidUrl: 'Invalid RSS feed URL',
-      networkIssue: 'Network problems, try again.',
+      networkSubmitIssue: 'Network problems, new feed was not added.',
+      networkUpdateIssue: 'Network problems, posts were not updated.',
     },
     alerts: {
       problem: 'Ouch...Houston, we’ve had a problem:',
-      success: 'Feed channel successfully added to the list',
+      finished: 'Feed channel successfully added to the list',
+      sending: 'Trying to add new feed',
     },
   },
 };
