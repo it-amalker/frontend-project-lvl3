f3a41b64908883b1c4ec6538fb65a47f37759801
amalker <it.amalker@yandex.ru>
2020-03-03 18:29:09 +0300

    add feed parse logic

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,27 @@
+import _ from 'lodash';
+
 export default (data) => {
   const parser = new DOMParser();
-  return parser.parseFromString(data, 'text/html');
+  const dom = parser.parseFromString(data, 'text/html');
+
+  const id = _.uniqueId();
+  const titleEl = dom.querySelector('title');
+  const descriptionEl = dom.querySelector('description');
+  const feed = { id, title: titleEl.innerText, description: descriptionEl.innerText };
+
+  const posts = [];
+  const postsItems = dom.querySelectorAll('item');
+  const reversedPosts = _.reverse([...postsItems]);
+  reversedPosts.forEach((post) => {
+    const postTitleEl = post.querySelector('title');
+    const postTitle = postTitleEl.innerText.replace('<![CDATA[', '').replace(']]>', '');
+    const postLinkEl = post.querySelector('a');
+    const postLinkElAlternative = post.querySelector('link');
+    const postLink = postLinkEl ? postLinkEl.href : postLinkElAlternative.innerText;
+    const postDateEl = post.querySelector('pubdate');
+    const postDate = postDateEl.innerText;
+    posts.push({ title: postTitle, link: postLink, date: postDate });
+  });
+
+  return [feed, posts];
 };
