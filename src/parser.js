1cb65126a7179af034f9209c4a09c269641e80e1
amalker <it.amalker@yandex.ru>
2020-03-10 09:31:06 +0300

    refactor parser

--- src/parser.js
+++ src/parser.js
@@ -1,27 +1,9 @@
-import _ from 'lodash';
-
-export default (data) => {
-  const parser = new DOMParser();
-  const dom = parser.parseFromString(data, 'text/html');
-
-  const id = _.uniqueId();
-  const titleEl = dom.querySelector('title');
-  const descriptionEl = dom.querySelector('description');
-  const feed = { id, title: titleEl.innerText, description: descriptionEl.innerText };
-
-  let posts = [];
-  const postsItems = dom.querySelectorAll('item');
-  const reversedPosts = _.reverse([...postsItems]);
-  reversedPosts.forEach((post) => {
-    const postTitleEl = post.querySelector('title');
-    const postTitle = postTitleEl.innerText.replace('<![CDATA[', '').replace(']]>', '');
-    const postLinkEl = post.querySelector('a');
-    const postLinkElAlternative = post.querySelector('link');
-    const postLink = postLinkEl ? postLinkEl.href : postLinkElAlternative.innerText;
-    const postDateEl = post.querySelector('pubdate');
-    const postDate = postDateEl.innerText;
-    posts = [...posts, { title: postTitle, link: postLink, date: postDate }];
-  });
-
-  return [feed, posts];
+export default (data, headers) => {
+  const isXmlData = headers['content-type'].includes('xml');
+  if (isXmlData) {
+    const parser = new DOMParser();
+    const dom = parser.parseFromString(data, 'text/html');
+    return dom.querySelector('channel');
+  }
+  throw new Error('Unsupported content type');
 };
